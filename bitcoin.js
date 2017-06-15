190f58521007410756ff91586aed9b119b305b4a
Michael Johnsey <michael.johnsey@mosaik.com>
2017-06-15 10:23:34 -0500

    Cleanup and error handling

--- bitcoin.js
+++ bitcoin.js
@@ -1,7 +1,7 @@
 const coinbase = require('coinbase');
-const http = require('https');
+const https = require('https');
 const _ = require('lodash');
-
+const bitbar = require('bitbar');
 
 // uses the bitfinex API http://docs.bitfinex.com/
 const baseApiUrl = 'https://api.bitfinex.com/v1';
@@ -12,12 +12,8 @@ const ethereumSymbol = 'Ξ';
 const bigmoneySymbol = '🤑';
 const bigmoneyAmount = 1245.0;
 
-// TIME_FORMAT_12H = '%I:%M %p'.freeze
 const timeFormat12h = '';
-// MM_DD_YYYY = '%m/%d/%Y'.freeze
 const mmddyyyy = '';
-// DATETIME_FORMAT = "#{MM_DD_YYYY} #{TIME_FORMAT_12H}".freeze
-const datetimeFormat = '';
 
 // # Coinbase API - https://www.coinbase.com/settings/api
 const coinbaseApiKey = process.env.COINBASE_API_KEY;
@@ -42,33 +38,41 @@ const getTickerUrl = function(baseUrl, symbol) {
 const tickerUrl = getTickerUrl(baseApiUrl, 'btcusd');
 const ethTickerUrl = getTickerUrl(baseApiUrl, 'ethusd');
 
-const addLine = function(newLine){
-  if(printString != ''){
-    printString += '\n';
+const handleCoinbaseResponse = function(account, data){
+  // { mid: '769.795', bid: '769.79', ask: '769.8', last_price: '769.8', low: '765.0', high: '772.72', volume: '3675.99258378', timestamp: '1481316120.69769026' }
+  const responseJson = JSON.parse(data);
+  let lastPrice = responseJson.last_price;
+  let lastPriceParsed = parseFloat(responseJson.last_price).toFixed(2);
+  let currentPrice = `${bitcoinSymbol} ${lastPriceParsed ? lastPriceParsed : lastPrice}`;
+  if(lastPriceParsed && lastPriceParsed > bigmoneyAmount){
+    currentPrice = `${currentPrice} ${bigmoneySymbol}`
   }
-  printString += `${newLine}\n`
-}
+  bitbar([
+    {
+      text: currentPrice,
+    },
+    bitbar.sep,
+    {text: 'Bitcoin'},
+    {text: `Wallet Balance: ${bitcoinSymbol}${account.balance.amount}`},
+    {text: `Wallet Balance: \$${account.balance.amount*responseJson.last_price}`},
+    bitbar.sep,
+    {text: 'Ether'},
+    {text: `Wallet Balance: ${ethereumSymbol}${'TODO'}`},
+    {text: `Wallet Balance: \$${'TODO'}`},
+    bitbar.sep,
+    {text: 'Coinbase', href: 'https://coinbase.com'},
+    {text: 'Coindesk', href: 'https://coindesk.com'},
+    {text: 'Coinigy', href: 'https://www.coinigy.com/main/markets/BITF/BTC/USD'},
+  ]);
+};
 
 client.getAccount(coinbaseAccountId, function(err, account) {
-  http.get(tickerUrl, function(res){
-    // addLine(JSON.stringify(res));
-    res.on('data', function (data) {
-      // { mid: '769.795', bid: '769.79', ask: '769.8', last_price: '769.8', low: '765.0', high: '772.72', volume: '3675.99258378', timestamp: '1481316120.69769026' }
-      const responseJson = JSON.parse(data);
-      // console.log(responseJson);
-      let lastPrice = responseJson.last_price;
-      let lastPriceParsed = parseFloat(responseJson.last_price).toFixed(2);
-      let currentPrice = `${bitcoinSymbol} ${lastPriceParsed ? lastPriceParsed : lastPrice}`;
-      if(lastPriceParsed && lastPriceParsed > bigmoneyAmount){
-        currentPrice = `${currentPrice} ${bigmoneySymbol}`
-      }
-      addLine(currentPrice);
-      addLine('---');
-      addLine(`Wallet Balance: ${account.balance.amount}`);
-      addLine(`Wallet Balance: \$${account.balance.amount*responseJson.last_price}`)
-      console.log(printString);
-  });
+  https.get(tickerUrl, (res) => {
+    res.on('data', (data) => {
+      return handleCoinbaseResponse(account, data);
+    });
 
+  }).on('error', (err) => {
+    console.error(err);
   });
-
 });
