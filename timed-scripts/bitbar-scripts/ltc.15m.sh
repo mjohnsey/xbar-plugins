ba3c1a7f15491c603e681b4a6752a84c8fd0b574
Michael Johnsey <mjohnsey@gmail.com>
2019-05-14 14:46:16 -0500

    Update ltc.15m.sh

--- timed-scripts/bitbar-scripts/ltc.15m.sh
+++ timed-scripts/bitbar-scripts/ltc.15m.sh
@@ -4,7 +4,7 @@ RUNNER="$BITBAR_SRC/crypto.js"
 node $RUNNER --accountId $COINBASE_LTC_ACCOUNT_ID \
     --coinbaseApiKey $COINBASE_API_KEY \
     --coinbaseApiSecret $COINBASE_API_SECRET \
-    --bigMoneyAmount $BIGMONEY \
+    --bigMoneyAmount ${LTC_BIGMONEY} \
     --gdaxApiKey $GDAX_API_KEY \
     --gdaxPassphrase $GDAX_API_PASSPHRASE \
     --gdaxSecret $GDAX_API_SECRET
