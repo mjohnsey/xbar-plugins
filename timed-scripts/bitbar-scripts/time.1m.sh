157b51fe73401465263723da402d09a3485f8f07
Michael Johnsey <mjohnsey@gmail.com>
2019-11-29 23:32:58 -0600

    Use the docker container for time

--- timed-scripts/bitbar-scripts/time.1m.sh
+++ timed-scripts/bitbar-scripts/time.1m.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ${HOME}/.aliases
-go-time
+/usr/local/bin/docker run --rm -v ${HOME}/.go-time.toml:/root/.go-time.toml docker.pkg.github.com/mjohnsey/go-time/go-time:amd64-latest
