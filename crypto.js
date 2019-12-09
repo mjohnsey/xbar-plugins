a6efb8aa1c179ddd46543ab11adfb5dcfca53652
Michael Johnsey <mjohnsey@gmail.com>
2019-12-09 10:34:29 -0600

    Remove old coinbase package in favor of gdax. Remove Gdax account query.
    Stopped using GDAX so no reason to keep polling empty accounts.

--- crypto.js
+++ crypto.js
@@ -1,8 +1,6 @@
 /* eslint no-console: ["error", { allow: ["error"] }] */
-const coinbase = require('coinbase');
 const _ = require('lodash');
 const bitbar = require('bitbar');
-const { promisify } = require('util');
 const minimist = require('minimist');
 const Gdax = require('coinbase-pro');
 
@@ -26,38 +24,21 @@ const parseCoinbaseAccount = function parseCoinbaseAccount(account) {
   cryptoTypes.push({ symbol: 'Ł', name: 'LTC' });
   cryptoTypes.push({ symbol: 'Ξ', name: 'ETH' });
   const result = {};
-  const accountBalance = account.balance.amount;
-  const accountNativeBalance = account.native_balance.amount;
+  const accountBalance = account.balance;
   const cryptoType = _.find(cryptoTypes, t => t.name === account.currency);
   result.currency = account.currency;
   result.symbol = !_.isUndefined(cryptoType) ? cryptoType.symbol : null;
   result.cryptoBalance = accountBalance;
-  result.usdBalance = accountNativeBalance;
   return result;
 };
 
 const getCurrentSpotPrice = async function getCurrentSpotPrice(coinbaseClient, currencyPair) {
-  const promisifiedGetSpotPrice = promisify(coinbaseClient.getSpotPrice.bind(coinbaseClient));
-  const spotPrice = await promisifiedGetSpotPrice({ currencyPair });
-  return spotPrice.data.amount;
+  const ticker = await coinbaseClient.getProductTicker(currencyPair);
+  return ticker.price;
 };
 
 const getCurrencyPair = function getCurrencyPair(currency, converted = 'USD') {
   return `${currency}-${converted}`;
-};
-
-const gdaxAccountByCurrency = function gdaxAccountByCurrency(accounts, currency) {
-  const acct = _.find(accounts, { currency });
-  return acct;
-};
-
-const parseGdaxAccount = function parseGdaxAccount(resp, currency) {
-  const gdaxAccounts = JSON.parse(resp.body);
-  const gdaxAccount = gdaxAccountByCurrency(gdaxAccounts, currency);
-  const gdaxBalance = roundMoney(gdaxAccount.balance, 5);
-  const gdaxAvail = roundMoney(gdaxAccount.available, 5);
-  const gdaxHold = roundMoney(gdaxAccount.hold, 5);
-  return { balance: gdaxBalance, available: gdaxAvail, hold: gdaxHold };
 };
 
 const main = async function main() {
@@ -70,29 +51,22 @@ const main = async function main() {
   });
   const bigMoneyAmount = args.bigMoneyAmount;
   const accountId = args.accountId;
-  const coinbaseApiKey = args.coinbaseApiKey;
-  const coinbaseApiSecret = args.coinbaseApiSecret;
 
   const gdaxApiKey = args.gdaxApiKey;
   const gdaxPassphrase = args.gdaxPassphrase;
   const gdaxSecret = args.gdaxSecret;
 
-  const client = new coinbase.Client({ apiKey: coinbaseApiKey, apiSecret: coinbaseApiSecret });
-  const promisifiedGetAccount = promisify(client.getAccount.bind(client));
+  const authedClient = new Gdax.AuthenticatedClient(gdaxApiKey, gdaxSecret, gdaxPassphrase);
 
-  const account = await promisifiedGetAccount(accountId);
+  const coinbaseAccounts = await authedClient.getCoinbaseAccounts();
+  const account = _.first(_.filter(coinbaseAccounts, acct => acct.id === accountId));
   const parsedAccount = parseCoinbaseAccount(account);
   const currencyPair = getCurrencyPair(parsedAccount.currency);
-  const currentPrice = await getCurrentSpotPrice(client, currencyPair);
+  const currentPrice = await getCurrentSpotPrice(authedClient, currencyPair);
+  parsedAccount.usdBalance = currentPrice * parsedAccount.cryptoBalance;
   const ppCurrentPrice = prettyPrintCurrentPrice(currentPrice,
     parsedAccount.symbol,
     bigMoneyAmount);
-
-  const authedClient = new Gdax.AuthenticatedClient(gdaxApiKey, gdaxSecret, gdaxPassphrase);
-  const promisifiedGetGdaxAccounts = promisify(authedClient.getAccounts.bind(authedClient));
-
-  const accountResp = await promisifiedGetGdaxAccounts();
-  const gdaxAccount = parseGdaxAccount(accountResp, parsedAccount.currency);
 
   bitbar([
     {
@@ -102,12 +76,8 @@ const main = async function main() {
     { text: parsedAccount.currency },
     { text: `Wallet Balance: ${parsedAccount.symbol} ${parsedAccount.cryptoBalance}` },
     { text: `Wallet Balance: $${parsedAccount.usdBalance}` },
-    { text: `GDAX Balance: ${parsedAccount.symbol} ${gdaxAccount.balance} - $${roundMoney(gdaxAccount.balance * currentPrice)}` },
-    { text: `GDAX Available: ${parsedAccount.symbol} ${gdaxAccount.available} - $${roundMoney(gdaxAccount.available * currentPrice)}` },
-    { text: `GDAX Hold: ${parsedAccount.symbol} ${gdaxAccount.hold} - $${roundMoney(gdaxAccount.hold * currentPrice)}` },
     bitbar.separator,
     { text: 'Coinbase', href: 'https://coinbase.com' },
-    { text: 'GDAX', href: `https://www.gdax.com/trade/${currencyPair}` },
   ]);
 };
 
